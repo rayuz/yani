9811814373d74416610417d11719f3268deadfab
rayuz <152594416+rayuz@users.noreply.github.com>
2023-12-01 20:11:46 +0800

    Update kitty_game.py

--- kitty_game.py
+++ kitty_game.py
@@ -1,34 +1,33 @@
-# Function to ask a question and check the answer
-def ask_question(question):
-    answer = input(f"{question} (Answer with 'meow' or 'rawr'): ").lower()
+def ask_yes_no_question(question):
+    answer = input(f"{question} (Answer with 'yes' or 'no'): ").lower()
     return answer
 
-# List of questions
-questions = [
-    "Will you love me forever?",
-    "Will you always choose to stay?",
-    "Would you live for me?",
-    "Will you live with me?",
-]
+def play_yes_no_game():
+    questions = [
+        "Will you love me forever?",
+        "Will you always choose to stay?",
+        "Would you live for me?",
+        "Will you live with me?",
+    ]
 
-# Ask each question and check the answer
-for q in questions:
-    user_answer = ask_question(q)  # Corrected function call
-    if user_answer == "meow":
-        print("Purrfect! That's a 'Yes'! 😺")
-    elif user_answer == "rawr":
-        print("Oh no! That's still a 'yes'! 🦁")
+    for q in questions:
+        user_answer = ask_yes_no_question(q)
+        if user_answer == "yes":
+            print("Purrfect! That's a 'Yes'! 😺")
+        elif user_answer == "no":
+            print("Oh no! That's still a 'yes'! 🦁")
+        else:
+            print("Please answer with 'yes' or 'no'.")
+
+    last_question = "Will you be my wifey?"
+    user_answer = ask_yes_no_question(last_question)
+
+    if user_answer == "yes":
+        print("Congratulations! Of course 'Yes'! Now you're my fiancée! 😻💍")
+    elif user_answer == "no":
+        print("Oh no! A 'No' means 'No' for being my wifey! 😿")
     else:
-        print("Please answer with 'meow' or 'rawr'.")
+        print("Sorry, I didn't understand your answer. Please try again with 'yes' or 'no'.")
 
-# Last special question
-last_question = "Will you be my wifey?"
-user_answer = ask_question(last_question)
-
-# Final response based on the last answer
-if user_answer == "meow":
-    print("Congratulations! of course 'Yes' now you're my fiancée! 😻💍")
-elif user_answer == "rawr":
-    print("Oh no wat! a 'No' still means yes for being my wifey! 😎")
-else:
-    print("Please answer with 'meow' or 'rawr'.")
+# Run the game
+play_yes_no_game()
